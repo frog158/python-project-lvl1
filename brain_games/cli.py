f475c011729708a8c7535dbb641350c4c46f77a8
Nikolay Markov <frog158@gmail.com>
2021-09-30 10:31:55 +0300

    remove error for linter

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,9 +1,10 @@
-"""This file add at step three"""
+"""This file add at step three."""
 
 import prompt
 
+
 def welcome_user():
-    """Welcome user. Ask his name and print"""
+    """Welcome user. Ask his name and print."""
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
